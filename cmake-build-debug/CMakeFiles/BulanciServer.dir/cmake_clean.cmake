1dde51eff38bf02715a811c05330f0cb6bef8333
Tobiáš Hládek <tobias.hladek@avast.com>
2019-12-28 23:25:35 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/BulanciServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BulanciServer.dir/main.cpp.o"
+  "BulanciServer.pdb"
+  "BulanciServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BulanciServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd;
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    char buffer[256];
+
+    if (argc < 2)
+    {
+        fprintf(stderr,"usage %s port\n", argv[0]);
+        return 1;
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error creating socket");
+        return 1;
+    }
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Error binding socket address");
+        return 2;
+    }
+
+    listen(sockfd, 5);
+    cli_len = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
+    if (newsockfd < 0)
+    {
+        perror("ERROR on accept");
+        return 3;
+    }
+
+    char* recieved = "START";
+    while( strcmp(recieved, "EXIT\n") != 0 ) {
+        bzero(buffer,256);
+        read(newsockfd, buffer, 255);
+        recieved = buffer;
+        printf("Here is the message: %s\n", recieved);
+        const char* msg = "I got your message";
+        write(newsockfd, msg, strlen(msg)+1);
+    }
+
+
+
+
+    close(newsockfd);
+    close(sockfd);
+
+    return 0;
+}
